d1be3ac38a1036ef43e97ce0908ddd0914add996
manga_osyo <manga.osyo@gmail.com>
2013-07-08 22:34:56 +0900

    Fix #image size.

--- web.rb
+++ web.rb
@@ -157,7 +157,7 @@ post '/gyazo' do
 		if /^#image[\s　]*(.+)/ =~ text
 			word = text[/^#image[\s　]*(.+)/, 1].split(/[\s　]/).map {|s| ERB::Util.url_encode s }.join("+")
 			url = "http://www.google.co.jp/search?&q=#{word}&tbm=isch"
-			post_lingr_gyazo(room, url, 0, 800, 140, 100)
+			post_lingr_gyazo(room, url, 0, 800, 140, 110)
 		end
 	}
 	return ""
