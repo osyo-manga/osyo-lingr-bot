dd642818b509048546533df8807731c94e790185
manga_osyo <manga.osyo@gmail.com>
2013-12-13 02:27:22 +0900

    Fix compile error.

--- web.rb
+++ web.rb
@@ -337,7 +337,7 @@ end
 def post_lingr_wandbox(room, code)
 	Thread.start do
 		result = compile(code)
-		result = result.empty? ? "Error" : result
+		result = result ? result : "Error"
 		post(room, "wandbox", result, ENV['WANDBOX_BOT_KEY'])
 	end
 end
